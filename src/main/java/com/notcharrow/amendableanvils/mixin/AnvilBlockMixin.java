25da1f512fa0110e93bb4bd9a9faa74ff87a8b36
NotchArrow <149845992+NotchArrow@users.noreply.github.com>
2025-05-15 21:04:43 -0400

    Switch to left-click

--- src/main/java/com/notcharrow/amendableanvils/mixin/AnvilBlockMixin.java
+++ src/main/java/com/notcharrow/amendableanvils/mixin/AnvilBlockMixin.java
@@ -1,19 +1,15 @@
 package com.notcharrow.amendableanvils.mixin;
 
-import it.unimi.dsi.fastutil.objects.Reference2ObjectArrayMap;
+import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.AnvilBlock;
-import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Items;
-import net.minecraft.server.MinecraftServer;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvents;
 import net.minecraft.state.property.Properties;
-import net.minecraft.util.ActionResult;
 import net.minecraft.util.BlockRotation;
-import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.world.World;
@@ -21,13 +17,13 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(AnvilBlock.class)
+@Mixin(AbstractBlock.class)
 public class AnvilBlockMixin {
-	@Inject(at = @At("HEAD"), method = "onUse")
-	private void init(BlockState state, World world, BlockPos pos, PlayerEntity player, BlockHitResult hit, CallbackInfoReturnable<ActionResult> cir) {
-		if (player.getMainHandStack().getItem() == Items.IRON_INGOT && state.getBlock() != Blocks.ANVIL) {
+	@Inject(at = @At("HEAD"), method = "onBlockBreakStart")
+	private void init(BlockState state, World world, BlockPos pos, PlayerEntity player, CallbackInfo ci) {
+		if (player.getMainHandStack().getItem() == Items.IRON_INGOT && state.getBlock() != Blocks.ANVIL
+		&& state.getBlock() instanceof AnvilBlock) {
 			Direction direction  = state.get(Properties.HORIZONTAL_FACING);
 			BlockRotation rotation = switch (direction) {
 				case EAST  -> BlockRotation.CLOCKWISE_90;
